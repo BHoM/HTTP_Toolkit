fb8ecca4b383cd1f4750e14dce069b73254c88c4
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-08-29 12:29:20 +0100

    remove useless usings

--- HTTP_Engine/Create/GetRequest.cs
+++ HTTP_Engine/Create/GetRequest.cs
@@ -21,14 +21,7 @@
  */
 
 using BH.oM.Base;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using BH.Engine.Reflection;
 using BH.oM.HTTP;
 
 namespace BH.Engine.HTTP
